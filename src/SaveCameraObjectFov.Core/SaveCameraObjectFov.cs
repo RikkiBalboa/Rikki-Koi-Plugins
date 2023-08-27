4fbac5bcea259e83c35c0ac4ffc76db3fe022268
RikkiBalboa <BalboaRikki+github@protonmail.com>
2023-08-27 09:02:17 +0200

    Move harmony patches to main class

--- src/SaveCameraObjectFov.Core/SaveCameraObjectFov.cs
+++ src/SaveCameraObjectFov.Core/SaveCameraObjectFov.cs
@@ -11,9 +11,11 @@ using System;
 using System.Linq;
 using UnityEngine;
 using HarmonyLib;
+using System.Collections.Generic;
 
 namespace Plugins
 {
+    [HarmonyPatch]
     [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
     internal class SaveCameraObjectFov : BaseUnityPlugin
     {
@@ -24,16 +26,52 @@ namespace Plugins
         internal static new ManualLogSource Logger;
         private readonly Harmony _harmony = new Harmony(PluginGUID);
 
+
+        internal static Dictionary<OCICamera, float> cameras = new Dictionary<OCICamera, float>();
+        internal static float mainFov = 23;
+        internal static OCICamera previousCamera;
+        internal static int previousCameraIndex = 0;
+        internal static int cameraIndex = 0;
+
         private void Awake()
         {
             Logger = base.Logger;
             _harmony.PatchAll();
         }
 
-        internal static void SetFOV(float fov)
+        private static void SetFOV(float fov)
         {
             Studio.Studio.Instance.cameraCtrl.cameraData.parse = fov;
             Studio.Studio.Instance.cameraCtrl.fieldOfView = fov;
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Studio.Studio), "ChangeCamera", new Type[] { typeof(OCICamera), typeof(bool), typeof(bool) })]
+        private static void ChangeCameraPostfix(OCICamera _ociCamera, bool _active)
+        {
+            if (previousCamera != null && cameras.ContainsKey(previousCamera))
+                cameras[previousCamera] = Studio.Studio.Instance.cameraCtrl.cameraData.parse;
+
+            SaveCameraObjectFov.Logger.LogInfo($"Switching Camera {cameraIndex}");
+            if (cameras.ContainsKey(_ociCamera))
+                SaveCameraObjectFov.SetFOV(cameras[_ociCamera]);
+            else
+                cameras[_ociCamera] = Studio.Studio.Instance.cameraCtrl.cameraData.parse;
+
+            if (cameraIndex == 0)
+                SaveCameraObjectFov.SetFOV(mainFov);
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(Studio.CameraSelector), "OnValueChanged")]
+        private static void OnValueChangedPrefix(int _index)
+        {
+            previousCamera = Studio.Studio.instance.ociCamera;
+            previousCameraIndex = cameraIndex;
+            cameraIndex = _index;
+
+            if (previousCameraIndex == 0)
+                mainFov = Studio.Studio.Instance.cameraCtrl.cameraData.parse;
         }
     }
 }
