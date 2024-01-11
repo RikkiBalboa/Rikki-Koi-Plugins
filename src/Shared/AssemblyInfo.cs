7ef5142c808e7f2f19b415e3c2f59b2b0262f1ff
RikkiBalboa <BalboaRikki+github@protonmail.com>
2024-01-11 01:08:37 +0100

    Remove unneeded code

--- src/Shared/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("https://github.com/DeathWeasel1337/Plugins")]
-[assembly: AssemblyCopyright("Copyright ©DeathWeasel1337 2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
