b86593d12b4e9d3d7ecd98469539e28602d07dd5
RikkiBalboa <BalboaRikki+github@protonmail.com>
2024-02-29 18:19:38 +0100

    Save/Load original values

---
+++ src/StudioSkinColor.Core/StudioSkinColor.SceneController.cs
@@ -0,0 +1,67 @@
+﻿using KKAPI.Studio.SaveLoad;
+using System;
+using System.Collections.Generic;
+using Studio;
+using KKAPI.Utilities;
+using ExtensibleSaveFormat;
+using MessagePack;
+using System.Linq;
+using static Plugins.StudioSkinColor;
+
+namespace Plugins
+{
+    internal class SceneController : SceneCustomFunctionController
+    {
+        protected override void OnSceneSave()
+        {
+            Logger.LogInfo("Saving Skin data");
+            var data = new PluginData();
+            Dictionary<int, ObjectCtrlInfo> dicObjectCtrl = Studio.Studio.Instance.dicObjectCtrl;
+            Dictionary<int, Dictionary<ModifiedValue, object>> defaultValues = new Dictionary<int, Dictionary<ModifiedValue, object>>();
+
+            foreach (int id in dicObjectCtrl.Where(x => x.Value is OCIChar).ToDictionary(k => k.Key, v => v.Value).Keys)
+            {
+                Logger.LogInfo($"Saving character {id}");
+                defaultValues[id] = DefaultValues[(OCIChar)dicObjectCtrl[id]];
+            }
+
+            if (defaultValues.Count > 0)
+            {
+                Logger.LogInfo($"Saving!");
+                // TODO Fix Color saving
+                data.data.Add("values", MessagePackSerializer.Serialize(defaultValues));
+            }
+            SetExtendedData(data);
+        }
+        protected override void OnSceneLoad(SceneOperationKind operation, ReadOnlyDictionary<int, ObjectCtrlInfo> loadedItems)
+        {
+            Logger.LogInfo($"Loading data");
+            if (operation == SceneOperationKind.Clear || operation == SceneOperationKind.Load)
+            {
+                Logger.LogInfo($"Clearing data");
+                DefaultValues.Clear();
+                if (operation == SceneOperationKind.Clear)
+                    return;
+            }
+
+            var data = GetExtendedData();
+            if (data?.data == null)
+            {
+                Logger.LogInfo($"No data to load");
+                return;
+            }
+
+            data.data.TryGetValue("values", out var defaultValuesOut);
+            if ((operation == SceneOperationKind.Load || operation == SceneOperationKind.Import) && defaultValuesOut != null)
+            {
+                Logger.LogInfo($"Loading data");
+                var defaultValues = MessagePackSerializer.Deserialize<Dictionary<int, Dictionary<ModifiedValue, object>>>((byte[])defaultValuesOut);
+                foreach (var entry in defaultValues)
+                {
+                    Logger.LogInfo($"Loading data for {entry.Key}");
+                    DefaultValues[(OCIChar)loadedItems[entry.Key]] = entry.Value;
+                }
+            }
+        }
+    }
+}
--- src/StudioSkinColor.Core/StudioSkinColor.cs
+++ src/StudioSkinColor.Core/StudioSkinColor.cs
@@ -3,10 +3,12 @@ using BepInEx.Configuration;
 using BepInEx.Logging;
 using KKAPI.Maker;
 using KKAPI.Studio;
+using KKAPI.Studio.SaveLoad;
 using KKAPI.Studio.UI;
-using Sirenix.Serialization.Utilities;
+using MessagePack;
 using Studio;
 using System;
+using System.Collections.Generic;
 using UniRx;
 using UnityEngine;
 using static Plugins.StudioSkinColor;
@@ -19,18 +21,47 @@ namespace Plugins
         public const string PluginGUID = "com.rikkibalboa.bepinex.studioSkinColor";
         public const string PluginName = "StudioSkinColor";
         public const string PluginNameInternal = Constants.Prefix + "_StudioSkinColor";
-        public const string PluginVersion = "0.1";
+        public const string PluginVersion = "0.2";
         internal static new ManualLogSource Logger;
+        public static ConfigEntry<KeyboardShortcut> KeyResetAll { get; private set; }
+
+        internal static Dictionary<OCIChar, Dictionary<ModifiedValue, object>> DefaultValues;
 
         private void Awake()
         {
             Logger = base.Logger;
+            KeyResetAll = Config.Bind(
+                "Keyboard Shortcuts", "Reset All",
+                new KeyboardShortcut(KeyCode.G, KeyCode.RightControl)
+            );
+            StudioSaveLoadApi.RegisterExtraBehaviour<SceneController>(PluginGUID);
+        }
+
+        private void Update()
+        {
+            if (KeyResetAll.Value.IsDown() && StudioAPI.InsideStudio)
+            {
+                foreach(var ociChar in DefaultValues.Keys)
+                {
+                    foreach (var modifiedValue in DefaultValues[ociChar])
+                    {
+                        switch (modifiedValue.Key.MethodType)
+                        {
+                            case MethodType.UpdateTexture:
+                                UpdateTextureColor((Color)modifiedValue.Value, (TextureColor)modifiedValue.Key.FieldEnum);
+                                break;
+                        }
+                    }
+                        
+                }
+            }
         }
 
         private void Start()
         {
             if (StudioAPI.InsideStudio)
             {
+                DefaultValues = new Dictionary<OCIChar, Dictionary<ModifiedValue, object>>();
                 RegisterStudioControls();
             }
         }
@@ -38,25 +69,28 @@ namespace Plugins
         private void RegisterStudioControls()
         {
             var catBody = StudioAPI.GetOrCreateCurrentStateCategory("Body");
-            catBody.AddControl(new CurrentStateColorSlider("Main Skin", c => c.GetChaControl().fileBody.skinMainColor, c => UpdateTextureColor(c, TextureColor.SkinMain)));
-            catBody.AddControl(new CurrentStateColorSlider("Sub Skin", c => c.GetChaControl().fileBody.skinSubColor, c => UpdateTextureColor(c, TextureColor.SkinSub)));
-            catBody.AddControl(new CurrentStateColorSlider("Tan", c => c.GetChaControl().fileBody.sunburnColor, c => UpdateTextureColor(c, TextureColor.Tan)));
-            catBody.AddControl(new CurrentStateCategorySwitch("Detail Line", c => c.GetChaControl().fileBody.drawAddLine)).Value.Subscribe(value => UpdateCustomBody(value, CustomBody.DrawAddLine));
-            catBody.AddControl(new CurrentStateCategorySlider("Detail Power", c => c.GetChaControl().fileBody.detailPower, 0, 1)).Value.Subscribe(value => UpdateCustomBody(value, CustomBody.DetailPower));
-            catBody.AddControl(new CurrentStateColorSlider("Pubic hair", c => c.GetChaControl().fileBody.underhairColor, value => UpdateCustomBody(value, CustomBody.PubicHairColor)));
-            catBody.AddControl(new CurrentStateColorSlider("Nipple", c => c.GetChaControl().fileBody.nipColor, value => UpdateCustomBody(value, CustomBody.NippleColor)));
-            catBody.AddControl(new CurrentStateCategorySlider("Nipple size", c => c.GetChaControl().fileBody.areolaSize, 0, 1)).Value.Subscribe(value => UpdateCustomBody(value, CustomBody.AreolaSize));
+            catBody.AddControl(new CurrentStateColorSlider("Main Skin", c => SetDefaultValue(c, MethodType.UpdateTexture, (int)TextureColor.SkinMain, c.GetChaControl().fileBody.skinMainColor), c => UpdateTextureColor(c, TextureColor.SkinMain)));
+            catBody.AddControl(new CurrentStateColorSlider("Sub Skin", c => SetDefaultValue(c, MethodType.UpdateTexture, (int)TextureColor.SkinSub, c.GetChaControl().fileBody.skinSubColor), c => UpdateTextureColor(c, TextureColor.SkinSub)));
+            catBody.AddControl(new CurrentStateColorSlider("Tan", c => SetDefaultValue(c, MethodType.UpdateTexture, (int)TextureColor.Tan, c.GetChaControl().fileBody.sunburnColor), c => UpdateTextureColor(c, TextureColor.Tan)));
+
+            catBody.AddControl(new CurrentStateCategorySwitch("Detail Line", c => SetDefaultValue(c, MethodType.UpdateCustomBody, (int)CustomBody.DrawAddLine, c.GetChaControl().fileBody.drawAddLine))).Value.Subscribe(value => UpdateCustomBody(value, CustomBody.DrawAddLine));
+            catBody.AddControl(new CurrentStateCategorySlider("Detail Power", c => SetDefaultValue(c, MethodType.UpdateCustomBody, (int)CustomBody.DetailPower, c.GetChaControl().fileBody.detailPower), 0, 1)).Value.Subscribe(value => UpdateCustomBody(value, CustomBody.DetailPower));
+            catBody.AddControl(new CurrentStateColorSlider("Pubic hair", c => SetDefaultValue(c, MethodType.UpdateCustomBody, (int)CustomBody.PubicHairColor, c.GetChaControl().fileBody.underhairColor), value => UpdateCustomBody(value, CustomBody.PubicHairColor)));
+            catBody.AddControl(new CurrentStateColorSlider("Nipple", c => SetDefaultValue(c, MethodType.UpdateCustomBody, (int)CustomBody.NippleColor, c.GetChaControl().fileBody.nipColor), value => UpdateCustomBody(value, CustomBody.NippleColor)));
+            catBody.AddControl(new CurrentStateCategorySlider("Nipple size", c => SetDefaultValue(c, MethodType.UpdateCustomBody, (int)CustomBody.AreolaSize, c.GetChaControl().fileBody.areolaSize), 0, 1)).Value.Subscribe(value => UpdateCustomBody(value, CustomBody.AreolaSize));
 
             var catBust = StudioAPI.GetOrCreateCurrentStateCategory("Bust");
-            catBust.AddControl(new CurrentStateCategorySlider("Softness", c => c.GetChaControl().fileBody.bustSoftness, 0, 1)).Value.Subscribe(f => UpdateBustSoftness(f, Bust.Softness));
-            catBust.AddControl(new CurrentStateCategorySlider("Weight", c => c.GetChaControl().fileBody.bustWeight, 0, 1)).Value.Subscribe(f => UpdateBustSoftness(f, Bust.Weight));
+            catBust.AddControl(new CurrentStateCategorySlider("Softness", c => SetDefaultValue(c, MethodType.UpdateBust, (int)Bust.Softness, c.GetChaControl().fileBody.bustSoftness), 0, 1)).Value.Subscribe(f => UpdateBustSoftness(f, Bust.Softness));
+            catBust.AddControl(new CurrentStateCategorySlider("Weight", c => SetDefaultValue(c, MethodType.UpdateBust, (int)Bust.Weight, c.GetChaControl().fileBody.bustWeight), 0, 1)).Value.Subscribe(f => UpdateBustSoftness(f, Bust.Weight));
 
             var catHair = StudioAPI.GetOrCreateCurrentStateCategory("Hair");
-            catHair.AddControl(new CurrentStateColorSlider("Color 1", c => c.GetChaControl().fileHair.parts[0].baseColor, color => UpdateHairColor(color, HairColor.Base)));
-            catHair.AddControl(new CurrentStateColorSlider("Color 2", c => c.GetChaControl().fileHair.parts[0].startColor, color => UpdateHairColor(color, HairColor.Start)));
-            catHair.AddControl(new CurrentStateColorSlider("Color 3", c => c.GetChaControl().fileHair.parts[0].endColor, color => UpdateHairColor(color, HairColor.End)));
-            catHair.AddControl(new CurrentStateColorSlider("Gloss", c => c.GetChaControl().fileHair.parts[0].glossColor, UpdateHairGlossColor));
-            catHair.AddControl(new CurrentStateColorSlider("Eyebrow", c => c.GetChaControl().fileFace.eyebrowColor, UpdateEyebrowColor));
+            catHair.AddControl(new CurrentStateColorSlider("Color 1", c => SetDefaultValue(c, MethodType.UpdateHairColor, (int)HairColor.Base, c.GetChaControl().fileHair.parts[0].baseColor), color => UpdateHairColor(color, HairColor.Base)));
+            catHair.AddControl(new CurrentStateColorSlider("Color 2", c => SetDefaultValue(c, MethodType.UpdateHairColor, (int)HairColor.Start, c.GetChaControl().fileHair.parts[0].startColor), color => UpdateHairColor(color, HairColor.Start)));
+            catHair.AddControl(new CurrentStateColorSlider("Color 3", c => SetDefaultValue(c, MethodType.UpdateHairColor, (int)HairColor.End, c.GetChaControl().fileHair.parts[0].endColor), color => UpdateHairColor(color, HairColor.End)));
+#if KKS
+            catHair.AddControl(new CurrentStateColorSlider("Gloss", c => SetDefaultValue(c, MethodType.UpdateHairColor, 0, c.GetChaControl().fileHair.parts[0].glossColor), color => UpdateHairColor(color, HairColor.Gloss)));
+#endif
+            catHair.AddControl(new CurrentStateColorSlider("Eyebrow", c => SetDefaultValue(c, MethodType.UpdateHairColor, 0, c.GetChaControl().fileFace.eyebrowColor), color => UpdateHairColor(color, HairColor.Eyebrow)));
         }
 
         private void UpdateTextures(ChaControl chaCtrl)
@@ -91,6 +125,19 @@ namespace Plugins
                 }
                 UpdateTextures(chaCtrl);
             }
+        }
+
+        private T SetDefaultValue<T>(OCIChar ociChar, MethodType methodType, int fieldEnum, T value)
+        {
+            if (!DefaultValues.ContainsKey(ociChar))
+                DefaultValues[ociChar] = new Dictionary<ModifiedValue, object>();
+
+            ModifiedValue modifiedValue = new ModifiedValue(methodType, fieldEnum);
+            if (DefaultValues[ociChar].ContainsKey(modifiedValue))
+                return value;
+
+            DefaultValues[ociChar][modifiedValue] = value;
+            return value;
         }
 
         private void UpdateCustomBody(object value, CustomBody customBody)
@@ -145,36 +192,33 @@ namespace Plugins
                 switch (hairColor)
                 {
                     case HairColor.Base:
-                        chaCtrl.fileHair.parts[0].baseColor = color;
+                        for(int i = 0;i < 4; i++)
+                            chaCtrl.fileHair.parts[i].baseColor = color;
                         break;
                     case HairColor.Start:
-                        chaCtrl.fileHair.parts[0].startColor = color;
+                        for (int i = 0; i < 4; i++)
+                            chaCtrl.fileHair.parts[i].startColor = color;
                         break;
                     case HairColor.End:
-                        chaCtrl.fileHair.parts[0].endColor = color;
+                        for (int i = 0; i < 4; i++)
+                            chaCtrl.fileHair.parts[i].endColor = color;
+                        break;
+#if KKS
+                    case HairColor.Gloss:
+                        for (int i = 0; i < 4; i++)
+                        {
+                            chaCtrl.fileHair.parts[i].glossColor = color;
+                            chaCtrl.ChangeSettingHairGlossColor(i);
+                        }
+                        break;
+#endif
+                    case HairColor.Eyebrow:
+                        chaCtrl.fileFace.eyebrowColor = color;
+                        chaCtrl.ChangeSettingEyebrowColor();
                         break;
                 }
-                chaCtrl.ChangeSettingHairColor(0, true, true, true);
-            }
-        }
-
-        private void UpdateHairGlossColor(Color color)
-        {
-            foreach (var cha in StudioAPI.GetSelectedCharacters())
-            {
-                var chaCtrl = cha.GetChaControl();
-                chaCtrl.fileHair.parts[0].glossColor = color;
-                chaCtrl.ChangeSettingHairGlossColor(0);
-            }
-        }
-
-        private void UpdateEyebrowColor(Color color)
-        {
-            foreach (var cha in StudioAPI.GetSelectedCharacters())
-            {
-                var chaCtrl = cha.GetChaControl();
-                chaCtrl.fileFace.eyebrowColor = color;
-                chaCtrl.ChangeSettingEyebrowColor();
+                for (int i = 0; i < 4; i++)
+                    chaCtrl.ChangeSettingHairColor(i, true, true, true);
             }
         }
 
@@ -205,6 +249,30 @@ namespace Plugins
             Base,
             Start,
             End,
+            Gloss,
+            Eyebrow,
+        }
+        public enum MethodType
+        {
+            UpdateTexture,
+            UpdateCustomBody,
+            UpdateBust,
+            UpdateHairColor,
+        }
+
+        [MessagePackObject]
+        public struct ModifiedValue
+        {
+            [Key(0)]
+            public MethodType MethodType { get; }
+            [Key(1)]
+            public int FieldEnum { get; }
+
+            public ModifiedValue(MethodType methodType, int fieldEnum)
+            {
+                MethodType = methodType;
+                FieldEnum = fieldEnum;
+            }
         }
     }
 }
