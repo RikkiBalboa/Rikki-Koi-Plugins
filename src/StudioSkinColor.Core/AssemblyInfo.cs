f70f81e6b89ddaeed6acf63eb8f064a722dfe78a
RikkiBalboa <141709004+RikkiBalboa@users.noreply.github.com>
2024-03-02 16:25:47 +0100

    New plugin: Studio Skin Color Control (#4)
    * Change body properties in studio
    
    * extract update to methods
    
    * consolidate some methods
    
    * KK support
    
    * Save/Load original values
    
    * Remove CustomBody options. Already handled by ME
    
    * Edit enum
    
    * remove saving/loading of reset value
    
    * Add timeline support
    
    * remove unneeded enum
    
    * Change plugin name

---
+++ src/StudioSkinColor.Core/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using Plugins;
+using System.Reflection;
+using static Plugins.StudioSkinColor;
+
+[assembly: AssemblyTitle(PluginNameInternal)]
+[assembly: AssemblyProduct(PluginNameInternal)]
+[assembly: AssemblyDescription(PluginName + " for " + Constants.GameName)]
+[assembly: AssemblyVersion(PluginVersion)]
+[assembly: AssemblyFileVersion(PluginVersion)]
