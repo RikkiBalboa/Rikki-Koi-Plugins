26dd2b92772f317dc8d5a93b7f0576bc611ed6b8
RikkiBalboa <BalboaRikki+github@protonmail.com>
2025-02-09 23:24:37 +0100

    prevent errors on switching outfits

--- src/StudioPseudoMaker.Core/Hooks.cs
+++ src/StudioPseudoMaker.Core/Hooks.cs
@@ -5,15 +5,17 @@ namespace Plugins
     internal class Hooks
     {
         [HarmonyPostfix, HarmonyPatch(typeof(ChaControl), nameof(ChaControl.ChangeCoordinateType), typeof(ChaFileDefine.CoordinateType), typeof(bool))]
+        [HarmonyWrapSafe]
         private static void ChangeCoordinateTypePostfix(ChaControl __instance)
         {
-            PseudoMaker.MainWindow.RefreshValues();
+            PseudoMaker.MainWindow?.RefreshValues();
         }
 
         [HarmonyPostfix, HarmonyPatch(typeof(ChaControl), nameof(ChaControl.ChangeCustomClothes))]
+        [HarmonyWrapSafe]
         private static void ChangeCustomClothesPostFix(ChaControl __instance)
         {
-            PseudoMaker.MainWindow.RefreshValues();
+            PseudoMaker.MainWindow?.RefreshValues();
         }
     }
 }
