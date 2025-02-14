fe42164c75e74c8476005a2095c206afc5c97f1f
RikkiBalboa <BalboaRikki+github@protonmail.com>
2025-02-14 23:37:32 +0100

    scroll to selected in dropdown

--- src/StudioPseudoMaker.Core/UI/Components/DropdownComponent.cs
+++ src/StudioPseudoMaker.Core/UI/Components/DropdownComponent.cs
@@ -24,6 +24,10 @@ namespace Plugins
             dropdown = GetComponentInChildren<Dropdown>(true);
 
             dropdown.onValueChanged.AddListener(idx => { if (!shouldNotUpdate) SetValueAction(idx); });
+
+            var scrollbar = dropdown.GetComponentInChildren<Scrollbar>(true);
+            var autoscroll = scrollbar.gameObject.AddComponent<AutoScrollToSelectionWithDropdown>();
+            autoscroll.Dropdown = dropdown;
         }
 
         private void Start()
@@ -43,6 +47,70 @@ namespace Plugins
                 shouldNotUpdate = true;
                 dropdown.value = GetCurrentValue();
                 shouldNotUpdate = false;
+            }
+        }
+
+        // Shamelessly stolen from https://github.com/IllusionMods/KK_Plugins/blob/master/src/MaterialEditor.Base/UI/UI.AutoScrollToCenter.cs
+        private class AutoScrollToSelectionWithDropdown : MonoBehaviour
+        {
+            public Dropdown Dropdown;
+            private bool autoScrolled;
+            private void OnEnable()
+            {
+                //No scrolling until LateUpdate when internal setup is complete
+                autoScrolled = false;
+            }
+
+            private void LateUpdate()
+            {
+                if (!autoScrolled)
+                {
+                    autoScrolled = true;
+                    AutoScroll();
+                }
+            }
+
+            private void AutoScroll()
+            {
+                if (Dropdown == null)
+                    return;
+
+                int items = Dropdown.options.Count;
+
+                if (items <= 1)
+                    return;
+
+                var scrollbar = GetComponent<Scrollbar>();
+
+                if (scrollbar == null)
+                    return;
+
+                //x = 0, y = 1
+                int axis = (scrollbar.direction < Scrollbar.Direction.BottomToTop ? 0 : 1);
+
+                var scrollRect = Dropdown.template.GetComponent<ScrollRect>();
+
+                float viewSize = scrollRect.viewport.rect.size[axis];
+                float itemSize = 20f;
+
+                if (Dropdown.itemText != null)
+                {
+                    var itemRect = (RectTransform)Dropdown.itemText.transform.parent;
+                    itemSize = itemRect.rect.size[axis];
+                }
+                else if (Dropdown.itemImage != null)
+                {
+                    var itemRect = (RectTransform)Dropdown.itemImage.transform.parent;
+                    itemSize = itemRect.rect.size[axis];
+                }
+
+                float viewAreaRatio = (viewSize / itemSize) / items;
+
+                float scroll = (float)Dropdown.value / items - viewAreaRatio * 0.5f;
+                scroll = Mathf.Clamp(scroll, 0f, 1f - viewAreaRatio);
+                scroll = Mathf.InverseLerp(0, 1f - viewAreaRatio, scroll);
+
+                scrollbar.value = Mathf.Clamp01(1.0f - scroll);
             }
         }
     }
