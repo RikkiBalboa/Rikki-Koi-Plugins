07f9b5175500fa51c79aacce22337da8f4619890
RikkiBalboa <141709004+RikkiBalboa@users.noreply.github.com>
2025-04-25 22:48:42 +0200

    [New Plugin] Butt Editor (#17)
    * New plugin: ButtEditor
    
    * Changed plugin name

---
+++ src/ButtEditor.Core/ButtEditor.cs
@@ -0,0 +1,60 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using KKAPI.Chara;
+using KKAPI.Maker;
+using KKAPI.Maker.UI;
+using System.Collections.Generic;
+using UniRx;
+
+namespace ButtEditor
+{
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+    public partial class ButtEditor : BaseUnityPlugin
+    {
+        public const string PluginGUID = "com.rikkibalboa.bepinex.buttEditor";
+        public const string PluginName = "Butt Editor";
+        public const string PluginNameInternal = Plugins.Constants.Prefix + "_ButtEditor";
+        public const string PluginVersion = "1.0";
+        internal static new ManualLogSource Logger;
+        private static Harmony harmony;
+
+        private ButtEditorCharaController buttEditorCharaController => MakerAPI.GetCharacterControl().gameObject.GetComponent<ButtEditorCharaController>();
+        
+        private void Awake()
+        {
+            Logger = base.Logger;
+            //harmony = Harmony.CreateAndPatchAll(typeof(Hooks));
+        }
+
+        private void Start()
+        {
+            CharacterApi.RegisterExtraBehaviour<ButtEditorCharaController>(PluginGUID);
+            MakerAPI.MakerBaseLoaded += OnEarlyMakerFinishedLoading;
+        }
+
+        private void OnEarlyMakerFinishedLoading(object sender, RegisterCustomControlsEvent e)
+        {
+            List<MakerSlider> sliderList = new List<MakerSlider>();
+            var category = MakerConstants.GetBuiltInCategory("01_BodyTop", "tglLower");
+
+            for (int i = 0; i < 4; i++)
+                sliderList.Add(AddSlider(e, category, (SliderType)i));
+        }
+
+        private MakerSlider AddSlider(RegisterCustomControlsEvent e, MakerCategory category, SliderType type)
+        {
+            var control = e.AddControl(new MakerSlider(category, $"Butt {type}", 0f, 3f, ButtEditorCharaController.defaultValues[type] * 10, this));
+            control.ValueChanged.Subscribe(Observer.Create<float>(value => buttEditorCharaController.SetButtValue(type, value / 10)));
+            control.Visible.OnNext(true);
+            return control;
+        }
+    }
+    public enum SliderType
+    {
+        Stiffness = 0,
+        Elasticity = 1,
+        Dampening = 2,
+        Weight = 3,
+    }
+}
---
+++ src/ButtEditor.Core/ButtEditorCharaController.cs
@@ -0,0 +1,78 @@
+﻿using ExtensibleSaveFormat;
+using KKAPI;
+using KKAPI.Chara;
+using MessagePack;
+using Shared;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ButtEditor
+{
+    internal class ButtEditorCharaController : CharaCustomFunctionController
+    {
+        public static Dictionary<SliderType, float> defaultValues = new Dictionary<SliderType, float>()
+        {
+            {SliderType.Stiffness, 0.04f},
+            {SliderType.Elasticity, 0.2f},
+            {SliderType.Dampening, 0.12f},
+            {SliderType.Weight, 0f},
+        };
+
+        public static Dictionary<SliderType, float> SavedValues = new Dictionary<SliderType, float>();
+
+        protected override void OnReload(GameMode currentGameMode, bool maintainState)
+        {
+            if (maintainState) return;
+
+            SavedValues.Clear();
+            var data = GetExtendedData();
+
+            if (data == null)
+                return;
+
+            if (data.data.TryGetValue(nameof(SavedValues), out var savedValues))
+            {
+                var values = MessagePackSerializer.Deserialize<Dictionary<SliderType, float>>((byte[])savedValues);
+                foreach (var value in values)
+                    SetButtValue(value.Key, value.Value);
+            }
+        }
+
+        protected override void OnCardBeingSaved(GameMode currentGameMode)
+        {
+            if (SavedValues.Count == 0)
+            {
+                SetExtendedData(null);
+            }
+            else
+            {
+                var data = new PluginData();
+                data.data.Add(nameof(SavedValues), MessagePackSerializer.Serialize(SavedValues));
+            }
+        }
+
+        public void SetButtValue(SliderType type, float value)
+        {
+            //ChaControl.reSetupDynamicBoneBust = true;
+
+            SavedValues[type] = value;
+
+            foreach (var _type in new ChaInfo.DynamicBoneKind[] { ChaInfo.DynamicBoneKind.HipL, ChaInfo.DynamicBoneKind.HipR })
+            {
+                if (ChaControl.dictDynamicBoneBust.TryGetValue(_type, out var _value))
+                    _value.ResetPosition();
+
+                if (type != SliderType.Weight)
+                    ChaControl.dictDynamicBoneBust[_type].setSoftParams(
+                        0,
+                        -1,
+                        SavedValues.GetValueOrDefault(SliderType.Dampening, defaultValues[SliderType.Dampening]),
+                        SavedValues.GetValueOrDefault(SliderType.Elasticity, defaultValues[SliderType.Elasticity]),
+                        SavedValues.GetValueOrDefault(SliderType.Stiffness, defaultValues[SliderType.Stiffness])
+                    );
+                else
+                    ChaControl.dictDynamicBoneBust[_type].setGravity(0, new Vector3(0, value / -100, 0));
+            }
+        }
+    }
+}
