ba3d71b01c3eed1993060879f10926aad76949f9
RikkiBalboa <BalboaRikki+github@protonmail.com>
2025-04-25 22:46:26 +0200

    New plugin: ButtEditor

---
+++ src/Shared/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared
+{
+    internal static class Extensions
+    {
+        public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue defaultValue)
+        {
+            return dictionary.TryGetValue(key, out var value) ? value : defaultValue;
+        }
+    }
+}
