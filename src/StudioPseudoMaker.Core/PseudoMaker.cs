54cf338eb2a2300568b8613311221fee167a2f47
RikkiBalboa <BalboaRikki+github@protonmail.com>
2025-05-29 13:57:43 +0200

    Increase version number

--- src/StudioPseudoMaker.Core/PseudoMaker.cs
+++ src/StudioPseudoMaker.Core/PseudoMaker.cs
@@ -40,7 +40,7 @@ namespace PseudoMaker
         public const string PluginGUID = "com.rikkibalboa.bepinex.studioPseudoMaker";
         public const string PluginName = "StudioPseudoMaker";
         public const string PluginNameInternal = Plugins.Constants.Prefix + "_StudioPseudoMaker";
-        public const string PluginVersion = "1.5.1.1";
+        public const string PluginVersion = "1.5.2.0";
         internal static new ManualLogSource Logger;
         private static Harmony harmony;
 
